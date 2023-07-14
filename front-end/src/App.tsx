7222e0fd732d98e3b300a741618673300273a37e
dBish6 <davidbish2002@hotmail.com>
2023-07-13 23:40:35 -0230

    Initial Front-end Commit

---
+++ front-end/src/App.tsx
@@ -0,0 +1,21 @@
+/* User Authentication Demo
+
+   Author: David Bishop
+   Creation Date: July 5, 2023
+*/
+
+import { useState } from "react";
+import "./App.css";
+
+// What back-end would you like to use; same front-end for every demo.
+
+function App() {
+  const [selectedBackEnd, setSelectedBackEnd] = useState({
+    javaSpring: true,
+    node: false,
+  });
+
+  return <></>;
+}
+
+export default App;
---
+++ front-end/src/components/partials/header/index.tsx
@@ -0,0 +1,13 @@
+const header = () => {
+  return (
+    <header>
+      <h1>Auth Demo</h1>
+      <div>
+        <img src="" alt="" />
+        <h2>{"Spring Boot Authentication"}</h2>
+      </div>
+    </header>
+  );
+};
+
+export default header;
