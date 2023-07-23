726091ed0d0a92c9132b33b9c2fcd81764d20b32
dBish6 <davidbish2002@hotmail.com>
2023-07-22 23:36:14 -0230

    Touch up and Author

--- back-ends/springBoot/src/main/java/com/demo/server/Application.java
+++ back-ends/springBoot/src/main/java/com/demo/server/Application.java
@@ -1,5 +1,11 @@
 package com.demo.server;
 
+/* Spring Boot User Authentication Demo Server
+
+   Author: David Bishop
+   Creation Date: July 13, 2023
+*/
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
