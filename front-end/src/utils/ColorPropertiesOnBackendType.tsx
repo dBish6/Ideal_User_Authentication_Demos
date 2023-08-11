2c788064bcded278bb940d4c7781f3f64e174982
dBish6 <davidbish2002@hotmail.com>
2023-08-10 22:20:16 -0230

    Dynamic Meta Tag

--- front-end/src/utils/ColorPropertiesOnBackendType.tsx
+++ front-end/src/utils/ColorPropertiesOnBackendType.tsx
@@ -12,11 +12,17 @@ const ColorPropertiesOnBackendType = () => {
       rootStyle.setProperty("--g400", "#7FC65B");
       rootStyle.setProperty("--g500", "#5FB832");
       rootStyle.setProperty("--g600", "#56A72E");
+      document
+        .querySelector('meta[name="theme-color"]')!
+        .setAttribute("content", "#5FB832");
     } else if (selectedBackEnd === "express") {
       rootStyle.setProperty("--g100", "#D0E2CF");
       rootStyle.setProperty("--g400", "#86B382");
       rootStyle.setProperty("--g500", "#68A063");
       rootStyle.setProperty("--g600", "#5F925A");
+      document
+        .querySelector('meta[name="theme-color"]')!
+        .setAttribute("content", "#68A063");
     }
   }, [selectedBackEnd]);
 };
