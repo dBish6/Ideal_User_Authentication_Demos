8e6e52c49397d85b64a7472550c4af9e43075c68
David <davidbish2002@hotmail.com>
2023-08-30 23:00:34 -0230

    Font-end; If deleted user is currentUser, logout.

--- front-end/src/api_services/DeleteUser.ts
+++ front-end/src/api_services/DeleteUser.ts
@@ -1,9 +1,11 @@
 import { User } from "../@types/User";
 import RequestHandler from "./AxiosInstance";
+import { useAuthContext } from "../contexts/AuthContext";
 import { useToastContext } from "../contexts/ToastContext";
 
 const DeleteUser = () => {
   const { instance } = RequestHandler(),
+    { currentUser, logOutUser } = useAuthContext(),
     addToast = useToastContext();
 
   const handleDelete = async (user: User) => {
@@ -12,6 +14,7 @@ const DeleteUser = () => {
       url: `/auth/user/${user.email}`,
     });
     if (res && res.status === 200) {
+      if (user.email === currentUser.user!.email) await logOutUser();
       addToast(`User ${user.displayName} successfully deleted.`, "success");
     }
   };
