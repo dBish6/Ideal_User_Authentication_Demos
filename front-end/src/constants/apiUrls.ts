718605550d25375e3cd86cd0b4f2fd5ce09c0bc3
David <davidbish2002@hotmail.com>
2023-09-14 21:57:04 -0230

    Front-end; Deployment urls.

--- front-end/src/constants/apiUrls.ts
+++ front-end/src/constants/apiUrls.ts
@@ -1,2 +1,8 @@
-export const springUrl = "http://localhost:8080/spring/api";
-export const expressUrl = "http://localhost:4000/express/api";
+export const springUrl =
+  process.env.NODE_ENV === "production"
+    ? "https://auth-demo-spring.fly.dev/spring/api"
+    : "http://localhost:8080/spring/api";
+export const expressUrl =
+  process.env.NODE_ENV === "production"
+    ? "https://auth-demo-express.fly.dev/express/api"
+    : "http://localhost:4000/express/api";
